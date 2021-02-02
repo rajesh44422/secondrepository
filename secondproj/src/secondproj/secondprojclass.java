83a5678c129c8ff7a357572323aef27af97d3422
ganeshmamidi123 <ganesh.m3456@gmail.com>
2021-02-02 15:38:16 +0530

    ddd

--- secondproj/src/secondproj/secondprojclass.java
+++ secondproj/src/secondproj/secondprojclass.java
@@ -2,6 +2,6 @@ package secondproj;
 
 public class secondprojclass {
    public static void main(String args []) {
-	   System.out.println("secondprojclass1..");
+	   System.out.println("secondprojclass1");
    }
 }
